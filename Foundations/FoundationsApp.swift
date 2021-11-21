d6ff5f3207cb5ce8f0ead871923c25dd0fa4830a
Jose Roberto <jrfsj@cin.ufpe.br>
2021-11-21 12:07:02 -0300

    Initial Commit

---
+++ Foundations/FoundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationsApp.swift
+//  Foundations
+//
+//  Created by Jose Roberto on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
